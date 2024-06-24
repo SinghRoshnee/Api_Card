fc508335060dc006b898441025924d8dc728903a
SinghRoshnee <singhroshni012001@gmail.com>
2024-06-24 18:21:28 +0530

    updated

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const btn = document.querySelector("button");
 const box = document.querySelector(".box");
 btn.addEventListener("click", (e) => {
   console.log(e);
-  box.style.height = " 450px";
-  box.style.width = " 470px";
+  box.style.height = "450px";
+  box.style.width = "470px";
   btn.style.display = "none";
 
   let requestUrl = "https://api.github.com/users/SinghRoshnee";
@@ -13,15 +13,20 @@ btn.addEventListener("click", (e) => {
     //   console.log(hmlrequest.readyState);
     if (hmlrequest.readyState === 4) {
       let edata = JSON.parse(this.responseText);
+
+      // Update the login paragraph
       document.querySelector(".login").innerHTML = `Username is ${edata.login}`;
       console.log(edata.login);
+
+      // Create the image element and insert it after the login paragraph
       var x = document.createElement("IMG");
       x.setAttribute("src", edata.avatar_url);
       x.setAttribute("width", "100");
       x.setAttribute("height", "100");
       x.style.margin = "20px";
-      box.appendChild(x);
-      console.log(`avatr is ${edata.avatar_url}`);
+      document.querySelector(".login").insertAdjacentElement("afterend", x);
+
+      console.log(`avatar is ${edata.avatar_url}`);
       document.querySelector(
         ".folowing"
       ).innerHTML = `Following are ${edata.following}`;
@@ -33,9 +38,8 @@ btn.addEventListener("click", (e) => {
 
       document.querySelector(
         ".repo"
-      ).innerHTML = `Repository link is :  ${edata.repos_url}`;
-
-      console.log(`repository  is  ${edata.repos_url}`);
+      ).innerHTML = `Number of Repositories is :  ${edata.public_repos}`;
+      console.log(`repository count is ${edata.public_repos}`);
     }
   };
 
